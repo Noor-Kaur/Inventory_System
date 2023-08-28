dd29839bdab4c6767a4029b4c6f0b32e7f219df5
Jatin Dhingra <jatindhingra.dhingra99@gmail.com>
2023-08-28 19:46:31 +0530

    adding a

--- src/Customer.java
+++ src/Customer.java
@@ -2,5 +2,7 @@ public class Customer
 {
     public static void main(String[] args) {
         System.out.println("customer from sources");
+        int a=5;
+        System.out.println(a);
     }
 }
